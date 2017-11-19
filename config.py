f7d009bceb0bad211375f8cdd05b5aea903394f3
Md Imran Hossain <yaimran21@gmail.com>
2017-11-19 12:46:43 +0600

    sublime text3 configaretion file

---
+++ config.py
@@ -0,0 +1,36 @@
+{
+	"always_show_minimap_viewport": true,
+	"auto_indent": true,
+	"bold_folder_labels": true,
+	"color_scheme": "Packages/User/SublimeLinter/iPlastic (SL).tmTheme",
+	"enable_tab_scrolling": true,
+	"folder_exclude_patterns":
+	[
+		"node_modules",
+		".svn",
+		".git",
+		".hg",
+		"CVS"
+	],
+	"font_face": "source code pro",
+	"font_size": 11,
+	"hide_sidebar_icons": true,
+	"ignored_packages":
+	[
+		"Vintage"
+	],
+	"line_numbers": true,
+	"line_padding_bottom": 1,
+	"line_padding_top": 2,
+	"overlay_scroll_bars": "enabled",
+	"save_on_focus_lost": true,
+	"theme": "ayu-light.sublime-theme",
+	"ui_big_tabs": false,
+	"ui_fix_tab_labels": true,
+	"ui_font_default": true,
+	"ui_font_size_small": true,
+	"ui_font_source_code_pro": true,
+	"ui_separator": true,
+	"ui_sidebar_highlight_row": true,
+	"ui_wide_scrollbars": true
+}
